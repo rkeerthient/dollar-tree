ad6c0429407ebec1ab9d45b4aa5a30a86dadd432
rkeerthient <rkeerthi@yext.com>
2023-08-12 01:15:02 +0530

    added slug

--- src/components/Department.tsx
+++ src/components/Department.tsx
@@ -3,44 +3,41 @@ import { useState } from "react";
 import { DirectoryItem } from "./DepartmentsType";
 
 interface TreeStructureProps {
-    data: DirectoryItem[];
+  data: DirectoryItem[];
 }
 interface TreeNodeProps {
-    label: string;
-    children: React.ReactNode;
-};
-const Department = ({ data }: TreeStructureProps) => {
-    return <div>{createTree(data)}</div>;
-
+  label: string;
+  children: React.ReactNode;
 }
+const Department = ({ data }: TreeStructureProps) => {
+  return <div>{createTree(data)}</div>;
+};
 
 export default Department;
 
-
-const createTree = (data: TreeStructureProps['data']) => {
-    return data.map((item) => (
-        <TreeNode key={item.slug} label={item.name}>
-            {item.dm_directoryChildren && createTree(item.dm_directoryChildren)}
-        </TreeNode>
-    ));
+const createTree = (data: TreeStructureProps["data"]) => {
+  return data.map((item) => (
+    <TreeNode key={item.slug} label={item.name}>
+      <a href={`/${item.slug}`}>
+        {item.dm_directoryChildren && createTree(item.dm_directoryChildren)}
+      </a>
+    </TreeNode>
+  ));
 };
 
 const TreeNode = ({ label, children }: TreeNodeProps) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleNode = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleNode = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div>
-            <div
-                className="cursor-pointer flex items-center"
-                onClick={toggleNode}
-            >
-                {isOpen ? '🔽' : '▶️'} {label}
-            </div>
-            {isOpen && <div className="ml-5">{children}</div>}
-        </div>
-    );
+  return (
+    <div>
+      <div className="cursor-pointer flex items-center" onClick={toggleNode}>
+        {isOpen ? "🔽" : "▶️"} {label}
+      </div>
+      {isOpen && <div className="ml-5">{children}</div>}
+    </div>
+  );
 };
