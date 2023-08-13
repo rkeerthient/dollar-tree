4acde72a39de8e9ab1fdcdefaa6309e50fb97570
rkeerthient <rkeerthi@yext.com>
2023-08-14 02:30:17 +0530

    added rating

--- src/components/cards/ProductCard.tsx
+++ src/components/cards/ProductCard.tsx
@@ -3,10 +3,14 @@ import { CardProps } from "@yext/search-ui-react";
 import * as React from "react";
 import { twMerge } from "tailwind-merge";
 import Ce_product from "../../types/products";
+import { StarIcon } from "@heroicons/react/20/solid";
 
 type ProductCardProps = CardProps<Ce_product> & {
   autocomplete?: boolean;
 };
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const ProductCard = ({ result, autocomplete }: ProductCardProps) => {
   const product = result.rawData;
@@ -16,16 +20,11 @@ const ProductCard = ({ result, autocomplete }: ProductCardProps) => {
     <div className="p-4 border flex flex-col gap-4">
       <div>
         {productImage && (
-          <Image
-            layout="fill"
-            height={288}
-            image={productImage} />
+          <Image layout="fill" height={288} image={productImage} />
         )}
       </div>
       <div className="pl-3">
-        <p className="text-xs font-bold h-4">
-          {product.brand?.toUpperCase()}
-        </p>
+        <p className="text-xs font-bold h-4">{product.brand?.toUpperCase()}</p>
         <p className="h-20">
           <a
             href={product.slug}
@@ -34,10 +33,27 @@ const ProductCard = ({ result, autocomplete }: ProductCardProps) => {
             {product.name}
           </a>
         </p>
+        <div className="ml-1 flex items-center">
+          {[0, 1, 2, 3, 4].map((rating) => (
+            <StarIcon
+              key={rating}
+              className={classNames(
+                product.c_rating > rating ? "text-green-500" : "text-gray-200",
+                "h-5 w-5 flex-shrink-0"
+              )}
+              aria-hidden="true"
+            />
+          ))}{" "}
+          <div>{product.c_rating}</div> <div>({product.c_noOfVotes})</div>
+        </div>
         <div className="flex justify-between items-center">
           <div className="text-gray-400 text-sm">Minimum You Can Buy: 4</div>
-          <div className="flex flex-col"><div className="text-xl text-green-500 font-bold">${product.price?.value}</div>
-            <div className="text-gray-700">per unit</div></div>
+          <div className="flex flex-col">
+            <div className="text-xl text-green-500 font-bold">
+              ${product.price?.value}
+            </div>
+            <div className="text-gray-700">per unit</div>
+          </div>
         </div>
       </div>
     </div>
