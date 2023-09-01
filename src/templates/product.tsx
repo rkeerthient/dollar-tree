abaabfb94438009ddd1fc06afc26ac4a8fdf7265
rkeerthient <rkeerthi@yext.com>
2023-09-01 18:25:40 +0530

    reviews fixed

--- src/templates/product.tsx
+++ src/templates/product.tsx
@@ -157,186 +157,179 @@ const Product: Template<TemplateRenderProps> = ({
                 : breadcrumbs
             }
           />
-          <div className="section">
-            <div className="bg-white">
-              <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                  <Tab.Group as="div" className="flex flex-col-reverse">
-                    {photoGallery && photoGallery.length >= 2 && (
-                      <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
-                        <Tab.List className="flex gap-4">
-                          {photoGallery.map((image: any) => (
-                            <Tab
-                              key={image.image.url}
-                              className="relative flex h-16 w-16 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
-                            >
-                              {({ selected }) => (
-                                <>
-                                  <span className="sr-only">{image.name}</span>
-                                  <span className="absolute inset-0 overflow-hidden rounded-md">
-                                    <img
-                                      src={image.image.url}
-                                      alt=""
-                                      className="h-full w-full object-cover object-center"
-                                    />
-                                  </span>
-                                  <span
-                                    className={classNames(
-                                      selected
-                                        ? "ring-indigo-500"
-                                        : "ring-transparent",
-                                      "pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2"
-                                    )}
-                                    aria-hidden="true"
+          <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 pb-16 sm:px-6 sm:pb-24 lg:max-w-7xl lg:px-8">
+              <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+                <Tab.Group as="div" className="flex flex-col-reverse">
+                  {photoGallery && photoGallery.length >= 2 && (
+                    <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+                      <Tab.List className="flex gap-4">
+                        {photoGallery.map((image: any) => (
+                          <Tab
+                            key={image.image.url}
+                            className="relative flex h-16 w-16 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
+                          >
+                            {({ selected }) => (
+                              <>
+                                <span className="sr-only">{image.name}</span>
+                                <span className="absolute inset-0 overflow-hidden rounded-md">
+                                  <img
+                                    src={image.image.url}
+                                    alt=""
+                                    className="h-full w-full object-cover object-center"
                                   />
-                                </>
-                              )}
-                            </Tab>
-                          ))}
-                        </Tab.List>
-                      </div>
-                    )}
-
-                    <Tab.Panels className="aspect-h-1 aspect-w-1 w-full">
-                      {photoGallery &&
-                        photoGallery.map((image: any) => (
-                          <Tab.Panel key={image.id}>
-                            <img
-                              src={image.image.url}
-                              alt={image.alt}
-                              className="h-full w-full object-cover object-center sm:rounded-lg"
-                            />
-                          </Tab.Panel>
-                        ))}
-                    </Tab.Panels>
-                  </Tab.Group>
-
-                  {/* Product info */}
-                  <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                    <div className="space-y-4">
-                      <h1 className="text-2xl font-semibold tracking-tight text-gray-900">
-                        {name}
-                      </h1>
-                      <div> SKU: {sku}</div>
-                      <div className="ml-1 flex items-center">
-                        {[0, 1, 2, 3, 4].map((rating) => (
-                          <StarIcon
-                            key={rating}
-                            className={classNames(
-                              c_rating > rating
-                                ? "text-green-500"
-                                : "text-gray-200",
-                              "h-5 w-5 flex-shrink-0"
+                                </span>
+                                <span
+                                  className={classNames(
+                                    selected
+                                      ? "ring-indigo-500"
+                                      : "ring-transparent",
+                                    "pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2"
+                                  )}
+                                  aria-hidden="true"
+                                />
+                              </>
                             )}
-                            aria-hidden="true"
+                          </Tab>
+                        ))}
+                      </Tab.List>
+                    </div>
+                  )}
+
+                  <Tab.Panels className="aspect-h-1 aspect-w-1 w-full">
+                    {photoGallery &&
+                      photoGallery.map((image: any) => (
+                        <Tab.Panel key={image.id}>
+                          <img
+                            src={image.image.url}
+                            alt={image.alt}
+                            className="h-full w-full object-cover object-center sm:rounded-lg"
                           />
-                        ))}{" "}
-                        <div>{c_rating}</div> <div>({c_noOfVotes})</div>
-                      </div>
+                        </Tab.Panel>
+                      ))}
+                  </Tab.Panels>
+                </Tab.Group>
+
+                {/* Product info */}
+                <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+                  <div className="space-y-4">
+                    <h1 className="text-2xl font-normal tracking-tight text-gray-900">
+                      {name}
+                    </h1>
+                    <div> SKU: {sku}</div>
+                    <div className="ml-1 flex items-center">
+                      {[0, 1, 2, 3, 4].map((rating) => (
+                        <StarIcon
+                          key={rating}
+                          className={classNames(
+                            c_rating > rating
+                              ? "text-green-500"
+                              : "text-gray-200",
+                            "h-5 w-5 flex-shrink-0"
+                          )}
+                          aria-hidden="true"
+                        />
+                      ))}{" "}
+                      <div>{c_rating}</div> <div>({c_noOfVotes})</div>
                     </div>
-                    <hr className="my-3" />
-                    <div className="mt-3">
-                      <h2 className="sr-only">Product information</h2>
-                      <div className="text-2xl font-bold">
-                        <span className="text-base font-light align-top">
-                          $
-                        </span>
-                        {price.value}{" "}
-                        <span className="text-base font-light mb-auto">
-                          each
-                        </span>
-                      </div>
+                  </div>
+                  <hr className="my-3" />
+                  <div className="mt-3">
+                    <h2 className="sr-only">Product information</h2>
+                    <div className="text-2xl font-bold">
+                      <span className="text-base font-light align-top">$</span>
+                      {price.value}{" "}
+                      <span className="text-base font-light mb-auto">each</span>
                     </div>
-                    <hr className="my-3" />
-                    {c_sets ? (
-                      <Dropdown price={price.value} sets={c_sets}></Dropdown>
-                    ) : (
-                      <div>
-                        <span className="text-base font-light align-top">
-                          $
-                        </span>
-                        {price.value}
-                        <span className="text-base font-light mb-auto">
-                          Total
-                        </span>
-                      </div>
+                  </div>
+                  <hr className="my-3" />
+                  {c_sets ? (
+                    <Dropdown price={price.value} sets={c_sets}></Dropdown>
+                  ) : (
+                    <div>
+                      <span className="text-base font-light align-top">$</span>
+                      {price.value}
+                      <span className="text-base font-light mb-auto">
+                        Total
+                      </span>
+                    </div>
+                  )}
+                  <hr className="my-3" />
+                  <Radio></Radio>
+                  <hr className="my-3" />
+                  <form className="mt-6">
+                    <div className="mt-10 flex">
+                      <button
+                        type="submit"
+                        className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-[#1e62c1] px-8 py-3  font-medium text-white  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full uppercase gap-6 text-lg"
+                      >
+                        <div>Add to bag</div>
+                        <BsFillCartFill />
+                      </button>
+                    </div>
+                  </form>
+                  <section aria-labelledby="details-heading" className="mt-12">
+                    <h2 id="details-heading" className="sr-only">
+                      Additional details
+                    </h2>
+                  </section>
+                </div>
+              </div>
+              <hr className="my-4" />
+              <div className="my-6">
+                <h3 className="text-[#198641] font-bold text-xl">
+                  Product Details
+                </h3>
+                <div className="text-sm">
+                  <RTF>
+                    {c_richTextDescription.replace(
+                      "Specifications",
+                      "\n **Specifications**\n"
                     )}
-                    <hr className="my-3" />
-                    <Radio></Radio>
-                    <hr className="my-3" />
-                    <form className="mt-6">
-                      <div className="mt-10 flex">
-                        <button
-                          type="submit"
-                          className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-[#1e62c1] px-8 py-3  font-medium text-white  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full uppercase gap-6 text-lg"
-                        >
-                          <div>Add to bag</div>
-                          <BsFillCartFill />
-                        </button>
-                      </div>
-                    </form>
-                    <section
-                      aria-labelledby="details-heading"
-                      className="mt-12"
-                    >
-                      <h2 id="details-heading" className="sr-only">
-                        Additional details
-                      </h2>
-                    </section>
-                  </div>
+                  </RTF>
                 </div>
-                <hr className="my-4" />
-                <div className="my-6">
-                  <h3 className="text-[#198641] font-bold text-xl">
-                    Product Details
-                  </h3>
-                  <div className="text-sm">
-                    <RTF>
-                      {c_richTextDescription.replace(
-                        "Specifications",
-                        "\n **Specifications**\n"
-                      )}
-                    </RTF>
-                  </div>
-                </div>
-                <hr className="my-4" />
-                <div className="mt-4">
-                  {dummyReviews.reviews.map((item: any, index: number) => (
-                    <div
-                      key={index}
-                      style={{
-                        padding: "2em",
-                        borderBottom: " 1px solid gray",
-                      }}
-                    >
+              </div>
+              <h3 className="text-[#198641] font-bold text-xl">
+                Rating & Reviews
+              </h3>
+              <hr className="my-4" />
+              <div className="mt-4">
+                {dummyReviews.reviews.map((item: any, index: number) => (
+                  <div
+                    key={index}
+                    className="px-8 py-3 border-b border-gray-600 text-sm"
+                    style={{
+                      padding: "2em",
+                      borderBottom: " 1px solid gray",
+                    }}
+                  >
+                    <div className=" flex items-center">
+                      {[0, 1, 2, 3, 4].map((rating) => (
+                        <StarIcon
+                          key={rating}
+                          className={classNames(
+                            item.rating > rating
+                              ? "text-[#198641]"
+                              : "text-gray-200",
+                            "h-5 w-5 flex-shrink-0"
+                          )}
+                          aria-hidden="true"
+                        />
+                      ))}
+                    </div>
+                    <div className="space-y-3 mt-3">
                       <div
                         className="flex"
                         style={{ justifyContent: "space-between" }}
                       >
-                        <div>{item.user}</div>
+                        <div className=" font-semibold">{item.user}</div>
                         <div>{item.date}</div>
                       </div>
-                      <div className="ml-1 flex items-center">
-                        {[0, 1, 2, 3, 4].map((rating) => (
-                          <StarIcon
-                            key={rating}
-                            className={classNames(
-                              c_rating > item.rating
-                                ? "text-green-500"
-                                : "text-gray-200",
-                              "h-5 w-5 flex-shrink-0"
-                            )}
-                            aria-hidden="true"
-                          />
-                        ))}
-                      </div>
-                      <div style={{ fontSize: `1.5em`, fontWeight: "400" }}>
-                        {item.title}
-                      </div>
-                      <div style={{ color: "#6f6f6f" }}>{item.comment}</div>
+                      <div className="text-xl font-semibold">{item.title}</div>
+                      <div>{item.comment}</div>
                     </div>
-                  ))}
-                </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
